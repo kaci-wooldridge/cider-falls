448de02ffbb6acdc3f64fa7fc3edaa087738d50d
Marek Snyder <mr.snyder31@gmail.com>
2022-12-07 12:54:29 -0500

    page layout set

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,20 @@
-import { parkList, serviceList } from "./database.js"
-import { parkNames } from "./parkAreas.js"
-import { serviceNames } from "./services.js"
-const parks = parkList();
-const services = serviceList()
-// ${parkNames()}
-// ${serviceNames()}
+import { areaServices } from "./services.js"
 
 
-const areaWithService = () => {}
 
-const areaServices = () => {
-    let eachAreaHtml = ``
-    
-    for (const area of parks) {
-        eachAreaHtml += `<div class="park" id="area-${area.id}"><h2 class="park-name"> ${area.name} </h2>`
-        if (Array.isArray(area.serviceId)){
-            for (let i=0; i<area.serviceId.length; i++){
-                for (const service of services) {
-                    if(service.id === area.serviceId[i]) {
-                         eachAreaHtml += `<li class="services">${service.name} </li>`
-                    }   
-                }
-            }
-        }  
-        eachAreaHtml += `</div>` 
-    }
-    return eachAreaHtml
-}
+
 
 const applicationHTML = `
+
 <section class="parks-services">
     ${areaServices()}
-</section>`
+</section>
+<section class="park-guest-list">
+<h2>Park Guests</h2>
+
+</section>
+
+`
 
 const mainContainer = document.querySelector(".container")
 mainContainer.innerHTML = applicationHTML
--- scripts/services.js
+++ scripts/services.js
@@ -2,28 +2,21 @@ import { parkList, serviceList } from "./database.js";
 const services = serviceList()
 const parks = parkList()
 
-//first pull out serviceIds from parks
-const pullServiceIds = () =>{
-    let serviceIdArray = []
-    for(const park of parks){
-        serviceIdArray.push(parseInt(park.serviceId))
-    }
-    return serviceIdArray
-}
-const pulledIds = pullServiceIds()
-
-
-export const serviceNames = () =>{
-    const services = serviceList()
-    let names= "provided services: "
-
-    const listItems = services.map(service =>{
-        let i = 0
-        while(pulledIds[i]){
-        return ` ${service.name}`
-        }
-    });
-    names += listItems.join(", ")
+export const areaServices = () => {
+    let eachAreaHtml = ``
     
-    return names
+    for (const area of parks) {
+        eachAreaHtml += `<div class="park" id="area-${area.id}"><h2 class="park-name"> ${area.name} </h2>`
+        if (Array.isArray(area.serviceId)){
+            for (let i=0; i<area.serviceId.length; i++){
+                for (const service of services) {
+                    if(service.id === area.serviceId[i]) {
+                         eachAreaHtml += `<li class="services">${service.name} </li>`
+                    }   
+                }
+            }
+        }  
+        eachAreaHtml += `</div>` 
+    }
+    return eachAreaHtml
 }
